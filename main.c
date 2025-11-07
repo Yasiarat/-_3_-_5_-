bf9126d3050c4ad9742f86672de40bba65fb14ac
Yasiarat <98023179+Yasiarat@users.noreply.github.com>
2025-11-07 20:28:25 +0300

    zezar_file.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// функция шифрования
 char encript(char *str, int k)
 {
     unsigned char symb = 0;
@@ -15,6 +16,7 @@ char encript(char *str, int k)
 
 }
 
+//функция расшифрования
 char decript(char *str, int k)
 {
     unsigned char symb = 0;
@@ -24,6 +26,52 @@ char decript(char *str, int k)
         str[i] = (char) symb;
     }
 
+}
+
+// Функция чтения файла
+char* read_file(const char *fn, long *f_sz)
+{
+    FILE *f = fopen(fn, "rb");
+    if (f == NULL) {
+        printf("Не удалось открыть файл %s для чтения\n", fn);
+        return NULL;
+    }
+
+    fseek(f, 0, SEEK_END);
+    *f_sz = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    char *str = (char*)malloc(*f_sz + 1);
+
+    if (str == NULL) {
+        printf("Не удалось выделить память\n");
+        fclose(f);
+        return NULL;
+    }
+
+    size_t len = fread(str, 1, *f_sz, f);
+    str[len] = '\0';
+
+    fclose(f);
+    return str;
+}
+
+// Функция для записи в файл
+int write_file(const char *fn, const char *str, long str_sz)
+{
+    FILE *f = fopen(fn, "wb");
+    if (f == NULL) {
+        printf("Не удалось открыть файл %s для записи\n", fn);
+        return 0;
+    }
+
+    size_t len = fwrite(str, 1, str_sz, f);
+    fclose(f);
+
+    if (len != str_sz) {
+        printf("Не удалось записать все данные в файл\n");
+        return 0;
+    }
 }
 
 int main()
